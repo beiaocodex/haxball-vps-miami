8641ebc9d8e9aaffdd941758e497eb5611114c13
beiaocodex <brettjuanmiguel@gmail.com>
2026-09-13 23:04:46 -0400

    Enhance Puppeteer launch configuration
    Added additional launch arguments for Puppeteer.

--- bot.js
+++ bot.js
@@ -2,11 +2,19 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   // 1. Iniciar navegador
-  const browser = await puppeteer.launch({
+ const browser = await puppeteer.launch({
     headless: "new",
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ]
   });
-
   // 2. Crear la página
   const page = await browser.newPage();
 
